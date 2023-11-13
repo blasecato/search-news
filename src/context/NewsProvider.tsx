0792d0ec2f206875d1a2d8b2b228db713b91b6ec
blasecato <bl.calderon@udla.edu.co>
2023-11-13 11:18:39 -0500

    sss

--- src/context/NewsProvider.tsx
+++ src/context/NewsProvider.tsx
@@ -40,7 +40,7 @@ const NewProvider = ({ children }: Props) => {
       }
       `;
       const { data } = await axios.get(url, {
-        headers: { "Accept-Encoding": "gzip,deflate,compress" },
+        headers: { "Accept-Encoding": "br" },
       });
       setNews(data.articles);
       setTotalNews(data.totalResults);
